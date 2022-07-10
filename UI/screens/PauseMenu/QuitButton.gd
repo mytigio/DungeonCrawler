279900d70efc25eb9896426ba3662d7c2e8aeb02
justin <joevegan@gmail.com>
2022-07-10 12:21:42 -0600

    pause overlay (escape menu)

--- UI/screens/PauseMenu/QuitButton.gd
+++ UI/screens/PauseMenu/QuitButton.gd
@@ -5,15 +5,15 @@ extends Button
 # var a = 2
 # var b = "text"
 
-
+var MULTIPLAYER_MENU = "res://UI/screens/MultiplayerSetup/MultiplayerSetup.tscn"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.connect("pressed", self, "_button_pressed")
 
-
 func _button_pressed():
-	get_parent().hide()
-	get_tree().paused = false
+	NetworkManager.networkDisconnect()
+	get_tree().change_scene(MULTIPLAYER_MENU)
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
