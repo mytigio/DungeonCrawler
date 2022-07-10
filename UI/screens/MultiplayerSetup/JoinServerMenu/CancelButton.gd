4c1429a24ad9a0b9d969ee07374f0edf27dd85a4
justin <joevegan@gmail.com>
2022-07-10 12:50:18 -0600

    wip

--- UI/screens/MultiplayerSetup/JoinServerMenu/CancelButton.gd
+++ UI/screens/MultiplayerSetup/JoinServerMenu/CancelButton.gd
@@ -4,16 +4,14 @@ extends Button
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-onready var scene = preload("res://UI/screens/MultiplayerSetup/JoinServerMenu/JoinServerMenu.tscn") 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.connect("pressed", self, "_button_pressed")
-	#createServerButton.connect("pressed", self, "_button_pressed")
-	
-	
+
 func _button_pressed():
-	get_tree().change_scene_to(scene)
+	#update this scene to be the lobby and wait for folks to join
+	get_tree().change_scene("res://UI/screens/MultiplayerSetup/MultiplayerSetup.tscn")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
